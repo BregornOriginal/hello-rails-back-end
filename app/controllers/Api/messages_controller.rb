2636979474380a1025ebf535c7c07ecc40209756
BregornOriginal <juliog_14@hotmail.com>
2022-10-18 19:50:48 -0300

    Added set_header on messages controller

--- app/controllers/Api/messages_controller.rb
+++ app/controllers/Api/messages_controller.rb
@@ -2,5 +2,13 @@ class Api::MessagesController < ApplicationController
   def index
     @greetings = Message.all.sample
     render json: @greetings
+
+    response.set_header('Access-Control-Allow-Origin', '*')
+
+    respond_to do |format|
+      format.html { render json: @greetings }
+      format.json { render json: @greetings }
+    end
+
   end
 end
